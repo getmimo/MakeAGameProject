706bb7c21b87e56e4575a4d7ad3a2f3180f31195
Radu Mester <radu@getmimo.com>
2018-04-11 19:45:18 +0200

    Create a Unity Game - Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,16 @@ public class PlayerController : MonoBehaviour {
     var rigidBody = GetComponent<Rigidbody2D> ();
     var transform = GetComponent<Transform> ();
     if (Input.GetKey ("right"))
-      rigidBody.velocity = new Vector2 (5,0);
+      rigidBody.velocity = new Vector2 (5,rigidBody.velocity.y);
     if (Input.GetKey ("left"))
-      rigidBody.velocity = new Vector2 (-5,0);
+      rigidBody.velocity = new Vector2 (-5,rigidBody.velocity.y);
     if (Input.GetKeyDown ("space"))
-      rigidBody.velocity = new Vector2 (0,10);
-    if (transform.position.y < -6)
-      transform.position = new Vector2 (-5,2);
+      rigidBody.velocity = new Vector2 (rigidBody.velocity.x,10);
+    if (transform.position.y < -6) 
+      if (transform.position.x < 2)
+        transform.position = new Vector2 (-5,2);
+    else
+        transform.position = new Vector2 (2,2);
+  
   }
 }
