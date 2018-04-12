6fa3e970b51bf81ee81a6654cd093944256d05ba
Radu Mester <radu@getmimo.com>
2018-04-12 13:55:57 +0200

    Extend the Unity Game - Enemy Movement - Moving left

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+  void Start () {
+  
+  }
+  
+  void Update () {
+    var rigidBody = GetComponent<Rigidbody2D> ();
+    rigidBody.velocity = new Vector2 (-5,rigidBody.velocity.y);
+  }
+}
