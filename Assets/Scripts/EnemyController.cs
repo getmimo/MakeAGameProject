82a6f9371917f2b0b31d44dbe15e4a6ae4e0a899
Radu Mester <radu@getmimo.com>
2018-04-15 11:50:03 +0200

    Extend the Unity Game - Enemy Movement - End of Fixed Update Lesson

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 
   public bool moveLeft;
+  public LayerMask groundLayer;
+  public Transform wallCheck;
+  public float wallCheckRadius;
+  private bool wallCollision;
   
   void Start () {
     moveLeft = true;
   }
+
+  void FixedUpdate () {
+  
+  }
   
   void Update () {
     var rigidBody = GetComponent<Rigidbody2D> ();
-    rigidBody.velocity = new Vector2 (-5,rigidBody.velocity.y);
+    if (moveLeft) {
+      rigidBody.velocity = new Vector2 (-5, rigidBody.velocity.y);
+    } else {
+        rigidBody.velocity = new Vector2 (5,rigidBody.velocity.y);
+    } 
   }
 }
